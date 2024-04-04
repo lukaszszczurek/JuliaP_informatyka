7864c2d3ea2a1a7f4fbc0952575c5af919d2d7b5
lukasz <lukaszszczurek02@gmail.com>
2024-04-04 20:32:33 +0200

    LEKCJA POWTÓRKOWA PĘTLE I TABLICE

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-       // pętla for (ile razy)
-        // pętla while (dopuki warunek jest prawdziwy);
-        // < to dopuki;
+        // przykład pętli for
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
 
-        int ileRazy = 14;
-        // napisz program, który wypisze pierwsze 14 liczb podzielnych przez
-        int ileTeraz =0;
-        int aktualnaliczba = 0;
-        while (ileTeraz < ileRazy) {
-            if (aktualnaliczba % 13 == 0) {
-                System.out.println(aktualnaliczba);
-                ileTeraz++;
-            }
+        // przykład pętli while
 
-            aktualnaliczba++;
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Hello World");
+            i++;
         }
 
     }
